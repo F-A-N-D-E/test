d442a6e5fe479037235526e513057123a9e5b13b
Andrey Lozhkin <andrey.lozhkin2015@yandex.ru>
2025-04-21 11:42:27 +0300

    Добавил ссылку на гит Изменил тайтл документа

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -4,7 +4,9 @@ import AppCSS from '../styles/App.module.css'
 export default function Footer() {
     return (<>
         <footer className={AppCSS.footer}>
-            <div className={AppCSS.footer_text}>Все права очень даже защищенны. Созданно <a href="https://vk.com/eshe_na_dne" target="_blanc">мною</a></div>
+            <div className={AppCSS.footer_text}>Все права очень даже защищенны. Созданно <a href="https://vk.com/eshe_na_dne" target="_blanc">мною. </a>
+                <a href="https://github.com/F-A-N-D-E/test" target="_blank">GitHab</a>
+            </div>
         </footer>
     </>)
 }
